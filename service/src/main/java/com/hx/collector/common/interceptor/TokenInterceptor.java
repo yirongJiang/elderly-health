e3ee4fce2f7a1261cae87a9e0c25b8dff1dc6147
wangjunjie <wjj121235847>
2022-09-28 16:05:03 +0800

    删除无用项

--- service/src/main/java/com/hx/collector/common/interceptor/TokenInterceptor.java
+++ service/src/main/java/com/hx/collector/common/interceptor/TokenInterceptor.java
@@ -1,7 +1,3 @@
-/**
- * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
- */
-
 package com.hx.collector.common.interceptor;
 
 import org.slf4j.LoggerFactory;
--- service/src/main/java/com/hx/collector/config/DruidConfig.java
+++ service/src/main/java/com/hx/collector/config/DruidConfig.java
@@ -1,8 +1,3 @@
-
-/**
- * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
- */
-
 package com.hx.collector.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
